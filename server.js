8a732065f2ada0de1b45ffd63593601787cb22c5
Axel Angel <axel+git@vneko.ch>
2014-07-18 21:15:37 +0200

    synchronize player positions, manage player models (join/part)
    Server 20 fps
    Client 60 fps

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: 8080})
-    , wsFreq = 1;
+    , wsFreq = 0.05;
 ;
 
 var NEXTID = 0;
@@ -13,7 +13,7 @@ function wsJson(ws, m_) {
 
 wss.on('connection', function(ws) {
     ws._id = NEXTID;
-    state[ws._id] = {
+    state.players[ws._id] = {
         position: {},
     };
     wsJson(ws, { type: 'id', data: ws._id });
@@ -21,15 +21,15 @@ wss.on('connection', function(ws) {
 
     ws.on('close', function () {
         console.log(['close', ws._id]);
-        delete state[ws._id];
+        delete state.players[ws._id];
     });
 
     ws.on('message', function(m_) {
         var m = JSON.parse(m_);
-        console.log(['message', ws._id, m]);
+        //console.log(['message', ws._id, m]);
         switch (m.type) {
             case 'position':
-                state[ws._id].position = m.data;
+                state.players[ws._id].position = m.data;
                 break;
         }
     });
@@ -40,7 +40,8 @@ wss.on('connection', function(ws) {
 setInterval(function () {
     for (var i in wss.clients) {
         wsJson(wss.clients[i], {
-            state: state,
+            type: 'state',
+            data: state,
         });
     }
 }, wsFreq*1000);
